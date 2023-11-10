817e973ff7308a5e3b7aedc33670b228f5bb4230
Gabriel Garcia-Miro Jeri <ggmj@Gabriels-MacBook-Pro-2.local>
2023-11-10 16:06:19 -0500

    arreglado

--- js/script.js
+++ js/script.js
@@ -5,51 +5,49 @@ let puntosOrdenador = 0;
 const botonJugada = document.querySelectorAll['boton-jugada']
 
 //for ( let i = 0; i >= 0; i++) {}
-document.getElementById('piedra').addEventListener('click',function(){
+document.getElementById('piedra').addEventListener('click', function () {
 
 })
-document.getElementById('papel').addEventListener('click',function(){
+document.getElementById('papel').addEventListener('click', function () {
 
 })
-document.getElementById('tijera').addEventListener('click',function(){
+document.getElementById('tijera').addEventListener('click', function () {
 
 })
 
 //botonJugada.forEach((boton) => {
-    boton.addEventListener('click', function(){
-        const seleccionPc = opciones[Math.floor(Math.random()*opciones.length)]
-      //const seleccionUsuario
-      const conseguirResultado = (seleccionPc,seleccionUsuario) => {
-        if (seleccionPc=seleccionUsuario){
+boton.addEventListener('click', function () {
+    const seleccionPc = opciones[Math.floor(Math.random() * opciones.length)]
+    //const seleccionUsuario
+    const conseguirResultado = (seleccionPc, seleccionUsuario) => {
+        if (seleccionPc = seleccionUsuario) {
             return 'es un empate'
 
-        } else if ( seleccionPc = 'piedra') {
-            if ( seleccionUsuario = 'papel'){
+        } else if (seleccionPc = 'piedra') {
+            if (seleccionUsuario = 'papel') {
                 return 'usuario gana'
             } else {
                 return 'pc gana'
             }
         } else if (seleccionPc = 'papel') {
-            if ( seleccionUsuario = 'tijera'){
+            if (seleccionUsuario = 'tijera') {
                 return 'usuario gana'
             } else {
                 return 'pc gana'
             }
         } else if (seleccionPc = 'tijera') {
-            if (seleccionUsuario = 'piedra'){
+            if (seleccionUsuario = 'piedra') {
                 return 'usuario gana'
             } else {
                 return 'pc gana'
             }
         }
-      }  
-      const sumaResultado = (resultado) => {
-      if (resultado = 'usuario gana'){
-        puntosUsuarios ++
-      }
     }
-      const resultado = conseguirResultado(seleccionPc,seleccionUsuario)
-      sumaResultado (resultado)
-    }) 
-    
+    const sumaResultado = (resultado) => {
+        if (resultado = 'usuario gana') {
+            puntosUsuarios++
+        }
+    }
+    const resultado = conseguirResultado(seleccionPc, seleccionUsuario)
+    sumaResultado(resultado)
 });
